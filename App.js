200a7c8fd941625fb9a142b48f10e26f813bc7c0
AndresBrenesGutierrez <60489852+and856@users.noreply.github.com>
2021-01-05 17:35:31 -0600

    AVANCE NUMERO BB

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity,Image, Button } from 'react-native';
 
 export default class App extends React.Component {
   state={
@@ -10,36 +11,37 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={styles.head}>
-        <Text style={styles.logo}>Seamos solución</Text>
+          <Image style={styles.imglogo} source = {require('./src/img/small.png')}/>
+          <Text style={styles.logo}>SEAMOS SOLUCION</Text>
         </View>
         
         <View style={styles.discurso}>
-        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur leo neque, eget aliquet ex varius eu. Curabitur eget augue turpis.</Text>
+          <Text>JUGUETES</Text>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur leo neque, eget aliquet ex varius eu.</Text>
         </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            style={styles.inputText}
-            placeholder="Email..." 
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({email:text})}/>
-        </View>
+        <Text>QUIERO OBSEQUIAR</Text>
         <View style={styles.inputView} >
           <TextInput  
             secureTextEntry
             style={styles.inputText}
-            placeholder="Contraseña" 
+            placeholder="Describe de manera corta el juguete" 
             placeholderTextColor="#AEADB3"
-            onChangeText={text => this.setState({password:text})}/>
+            onChangeText={text => this.setState({descripcion:text})}/>
         </View>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>¿Olvidaste tu contraseña?</Text>
+        
+        <TouchableOpacity style={styles.ingresar}>
+          <Text style={styles.txtIngresar}>ENVIAR</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.loginText}>Signup</Text>
-        </TouchableOpacity>
+        
+        
+        <View style={styles.footer}>
+          <Text style={{color:'white'}} >Conoce mas en:</Text>
+          <View style={styles.socialMedia}>
+            <TouchableOpacity>
+              <Image source = {require('./src/img/SM_FBsmall.png')}/>
+            </TouchableOpacity>
+          </View>
+        </View>
 
   
       </View>
@@ -50,58 +52,110 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFBCC',
+    textAlign: 'center',
+    //backgroundColor: '#FFFBCC',
     alignItems: 'center',
     justifyContent: 'center',
   },
   head: {
-    height: "10%",
-    backgroundColor: "#A41D00"
-
+    width: '100%',
+    textAlign: 'center',
+    backgroundColor: "#4573FF",
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    paddingTop: 30
+  },
+  imglogo:{
+    width: '10%'
   },
   logo:{
-    fontWeight:"bold",
+    fontWeight:'bold',
     fontSize:30,
-    color:"#fb5b5a",
-    marginBottom:40
+    color:'white',
+    paddingTop:5,
+    paddingBottom: 10
   },
   discurso:{
-    width: "80%",
-    marginHorizontal:10,
-    backgroundColor:"#fb5b5a",
-    textAlign:"center",
-    alignItems: "center"
+    width: '80%',
+    textAlign:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10
   },
   inputView:{
     width:"80%",
-    backgroundColor:"#D1D1D1",
-    borderRadius:15,
+    backgroundColor:"#FFFFFF",
+    borderRadius:10,
     borderWidth:1,
     borderColor:"#606060",
     height:50,
-    marginBottom:20,
+    marginBottom:10,
     justifyContent:"center",
     padding:20
   },
   inputText:{
     height:50,
-    color:"white"
+    color:"black"
   },
-  forgot:{
+  ingresar:{
     color:"white",
-    fontSize:11
+    fontSize:20,
+    backgroundColor:"#45DCFF",
+    borderRadius:10,
+    height:50,
+    paddingHorizontal: 10,
+    alignItems:"center",
+    justifyContent:"center",
   },
-  loginBtn:{
+  txtIngresar:{
+    color:'#FFFFFF',
+    fontSize: 20,
+    fontWeight:"bold"
+  },
+  registroBtn:{
     width:"80%",
-    backgroundColor:"#fb5b5a",
-    borderRadius:25,
+    backgroundColor:"white",
+    borderRadius:10,
     height:50,
     alignItems:"center",
     justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
+    marginTop:5,
+    marginBottom:10,
+    shadowColor: "#000",
+    shadowOffset: {
+	    width: 0,
+	    height: 2,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.00,
+    elevation: 3,
   },
-  loginText:{
-    color:"#A41D00"
+  registroText:{
+    color:"#71DCFF",
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+  footer:{
+    width: '100%',
+    backgroundColor: "#4573FF",
+    flex:1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginTop: 10,
+    paddingTop: 1,
+    paddingBottom:5
+  },
+  footerTex:{
+    fontSize:20,
+    color:'white'
+  },
+  socialMedia:{
+    height:41,
+    flex:1,
+    justifyContent: 'center',
+    alignItems:'center'
+    
   }
 });
